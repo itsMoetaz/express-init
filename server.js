c168ae4ceaa2f0833a309c0c539fbd8b9ed7ca89
moetaz <moetaz.benkhedher@esprit.tn>
2024-12-03 16:32:03 +0100

    create & read

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express')
 var userRouter = require('./controller/userController')
+var mongoose = require ('mongoose')
 var app= express()
 app.use(express.json())
+mongoose.connect('mongodb://localhost:27017/user-db').then(
+    ()=>{console.log("connected");
+})
+.catch((error)=>{
+    console.log(error);
+})
+
 app.use('/users', userRouter)
 var http = require('http')
 var server = http.createServer(app)
+
 server.listen(3000,()=>{
     console.log('server started !');
 })
--- service/userService.js
+++ service/userService.js
@@ -1,13 +1,27 @@
-function list(req,res,next){
-    res.end('User List')
+var User = require ('../model/userModel');
+async function list(req,res,next){
+    await User.find()
+    .then((data,err)=>{
+        if(err){
+        res.status(503).json(err)}
+        else{
+            res.status(200).json(data)
+        }
+    })
 }
 
-const create = (req,res,next)=>{
+const create = async (req,res,next)=>{
     const { nom, email } = req.body 
     console.log(req.body.nom);
     console.log(req.params.age)
     const { age } = req.params
     console.log(req.params);
+
+    await new User ({
+        nom:nom,
+        email:email,
+        age:age}).save().then((err,data)=>{if(err){console.log(err);
+        }})
     
 res.json('User added ! nom : '+ nom + ' email : '+ email+ ' age : '+ age)
 }
