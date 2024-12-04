7d69122756c5022c777adc8ef484aa6e88483c04
moetaz <moetaz.benkhedher@esprit.tn>
2024-12-04 16:40:25 +0100

    update & delete

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var router = express.Router()
-const { list, create } = require('../service/userService')
+const { list, create, supp, modifier } = require('../service/userService')
 var validate = require('../middleware/validation')
 
 
 router.get('/list',list)
 router.post('/create/:age',validate,create)
+router.delete('/delete/:id',supp)
+router.put('/update/:id',modifier)
 
 module.exports = router
--- service/userService.js
+++ service/userService.js
@@ -9,6 +9,28 @@ async function list(req,res,next){
         }
     })
 }
+async function supp(req,res,next){
+    await User.findByIdAndDelete(req.params.id)   
+     .then((data,err)=>{
+        if(err){
+        res.status(503).json(err)}
+        else{
+            res.status(200).json(data)
+        }
+    })
+
+}
+
+async function modifier(req,res,next){
+    await User.findByIdAndUpdate(req.params.id,req.body,{new: true})
+    .then((data,err)=>{
+        if(err){
+        res.status(503).json(err)}
+        else{
+            res.status(200).json(data)
+        }
+    })
+}
 
 const create = async (req,res,next)=>{
     const { nom, email } = req.body 
@@ -26,4 +48,4 @@ const create = async (req,res,next)=>{
 res.json('User added ! nom : '+ nom + ' email : '+ email+ ' age : '+ age)
 }
 
-module.exports = { create, list }
+module.exports = { create, list, supp, modifier }
